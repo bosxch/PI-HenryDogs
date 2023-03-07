1113b93153634498dfc8eb17fdfd85814690257c
bosxch <amparocardosobosch@gmail.com>
2023-03-07 13:01:03 -0300

    imports

--- client/src/App.js
+++ client/src/App.js
@@ -2,9 +2,9 @@ import "./App.css";
 import React from "react";
 import { Route, Switch } from "react-router-dom";
 import Home from "./Components/Home/Home";
-import DogDetail from "./Components/DogDetail/DogDetail";
-import LandingPage from "./Components/LandingPage/LandingPage";
-import CreateDog from "./Components/CreateDog/CreateDog";
+import DogDetail from "./components/DogDetail/DogDetail";
+import LandingPage from "./components/LandingPage/LandingPage";
+import CreateDog from "./components/CreateDog/CreateDog";
 import axios from 'axios';
 
 axios.defaults.baseURL = 'http://localhost:3001';
