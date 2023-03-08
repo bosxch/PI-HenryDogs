8c81042d36934dc0ce2b18f46ab2964cd1c5da9c
bosxch <amparocardosobosch@gmail.com>
2023-03-07 22:41:52 -0300

    deploying

--- api/src/db.js
+++ api/src/db.js
@@ -10,6 +10,8 @@ const {
 const sequelize = new Sequelize(`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/${DB_NAME}`, {
   logging: false, // set to console.log to see the raw SQL queries
   native: false, // lets Sequelize know we can use pg-native for ~30% more speed
+  dialect: 'mysql',
+  host: DB_HOST,
 });
 const basename = path.basename(__filename);
 
